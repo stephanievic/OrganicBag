db94e57bc42e7fe390d8c08b668289e06060ba25
Stephanie Victoria <stephanievictoria.dossantos@gmail.com>
2023-11-03 23:30:09 -0300

    funcinalidade carrinho

--- organicbag/src/main/webapp/js/index.js
+++ organicbag/src/main/webapp/js/index.js
@@ -1,22 +1,61 @@
 let menu = document.getElementById("menu-nav");
-let iconCart = document.querySelector("#icon-cart");
+const iconCart = document.querySelector("#icon-cart");
 let cart = document.querySelector("#cart");
 let qtdItem = document.querySelector(".qtd-item");
-let closeCart = document.querySelector("#close-cart");
+const closeCart = document.querySelector("#close-cart");
+let countItem = document.getElementsByClassName("count-item");
+let count = 1;
+//const removeItem = document.querySelector(".remove-button");
+//const addItem = document.querySelector(".add-button");
 
-//contador do carrinho
-function countProduct(id) {
-  
+function changeMenuBackground() {
+  //evento para alterar background do menu de acordo com scroll
+  document.addEventListener("scroll", () => {
+    var positionY = window.pageYOffset;
+
+    menu.style.backgroundColor = positionY <= 700 ? "transparent" : "#8fa793";
+  });
 }
 
-//evento para alterar background do menu de acordo com scroll
-document.addEventListener("scroll", () => {
-  var positionY = window.pageYOffset;
+//contador do carrinho
+function removeAddItem(button) {
+    let divButton = button.parentElement;
+    let input = divButton.children[1];
+    
+    
+    if (button.classList.contains("remove-button")){
+      count--;
+    }
 
-  menu.style.backgroundColor = positionY <= 700 ? "transparent" : "#8fa793";
-});
+    else if (button.classList.contains("add-button")){
+      count++;      
+    }
 
-//mostrar e fechar carrinho
-iconCart.addEventListener("click", () => {cart.classList.remove("hide")});
-closeCart.addEventListener("click", () => {cart.classList.add("hide")});
+    if (count == 0){
+      divButton.parentElement.remove()
+    }
 
+    input.value = count;
+    
+}
+
+//eventos e chamadas de métodos
+function Main() {
+  changeMenuBackground();
+
+  for(i = 0; i < countItem.length; i++){
+    countItem[i].addEventListener("click", (e) => {
+      //e.preventDefault();
+      removeAddItem(e.currentTarget); 
+     
+    })
+  }
+
+  
+  //mostrar e fechar carrinho
+  iconCart.addEventListener("click", () => cart.classList.remove("hide"));
+  closeCart.addEventListener("click", () => cart.classList.add("hide"));
+
+}
+
+Main();
